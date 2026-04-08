8d633ef93e8e390b4df5e95c92a21446d1332cff
sbailleul <bailleul.sacha@gmail.com>
2026-04-09 00:47:49 +0200

    feat: add actix-cors dependency and configure CORS settings in main application

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http, web, App, HttpServer};
 use garden::adapters::outbound::memory::vegetable_repository::InMemoryVegetableRepository;
 use garden::application::ports::vegetable_repository::VegetableRepository;
 
@@ -8,7 +9,14 @@ async fn main() -> std::io::Result<()> {
     let repo: Box<dyn VegetableRepository> = Box::new(InMemoryVegetableRepository);
     let repo_data = web::Data::new(repo);
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:5173")
+            .allowed_origin("http://127.0.0.1:5173")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![http::header::CONTENT_TYPE, http::header::ACCEPT])
+            .max_age(3600);
         App::new()
+            .wrap(cors)
             .app_data(repo_data.clone())
             .configure(garden::adapters::inbound::http::routes::configure)
             .app_data(web::JsonConfig::default().error_handler(|err, _req| {
